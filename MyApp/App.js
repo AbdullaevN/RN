2c6fd4ce67afc0e2647e6f2ab74979efb907bc1a
AbdullaevN <nurlanabdullaev9820@gmail.com>
2020-11-20 14:36:04 +0600

    Stylesheet

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,77 +1,82 @@
 import React from 'react'
-import {Text, View, ScrollView} from 'react-native'
-import Card from './components/Card'
-// import MyFuncComponent from './components/MyFuncComponent'
-// import MyArrowFuncComponent from './components/MyArrowFuncComponent'
-// import MyClassComponent from './components/MyClassComponent'
-// import MyOSCompanent from './components/MyOSComponent/MyOSComponent'
+import {View, Text, StyleSheet, SafeAreaView, Platform, StatusBar} from 'react-native'
 
-// const App = () => {
-//   return(
-//     <View style ={styles.container}>
-//       <ScrollView>
-//       <MyFuncComponent/>  
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <MyFuncComponent/>
-//       <MyArrowFuncComponent/>
-//       <MyClassComponent/>
-//       <MyOSCompanent/>
-//       <Text>Open up App.js to start</Text>
-//       </ScrollView>
-//     </View>
-//   )
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   }
-// })
-// export default App
-
-const App = () => {
+export default function App() {
     return(
-        <View>
-            <ScrollView>
-                <Card userName = {"Игорь"} />
-            </ScrollView>
-        </View>
+        <SafeAreaView style = {styles.container}>
+            <View style={styles.item}>
+            <Text>1</Text>
+            </View>
+
+            <View style={[styles.item, styles.item2]}>
+            <Text>2</Text>
+            </View>
+
+            <View style={styles.item}>
+                <Text>3</Text>
+                    <View style={styles.item33}>
+                    <Text style={styles.text}>
+                        33
+                        <Text>333</Text>
+                        </Text>
+                </View>
+            </View>
+
+            <View style={styles.item}>
+                <Text>4</Text>
+                <View style={styles.item44}>
+                    <Text>44</Text>
+            </View>
+            </View> 
+
+            <View style={styles.item}>
+            <Text>5</Text>
+            </View>
+        </SafeAreaView>
     )
 }
-export default App
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#96ceb4',
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight: 0
+    },
+    item:{
+        flex: 1,
+        // justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffeead',
+        borderBottomWidth: StyleSheet.hairlineWidth
+    },
+    item2: {
+        backgroundColor: '#d9534f',
+        transform: [
+            {translateX: 150 },
+            {rotateZ: '0.8rad'}
+        ]
+    },
+    item33:{
+        backgroundColor: '#293462',
+        ...StyleSheet.absoluteFill
+    },
+    text: {
+        color: '#fff'
+    },
+    item44:{
+        position: 'absolute',
+        ...Platform.select({
+            ios:{
+                top: 0,
+                left: 0
+            },
+            android: {
+                top: 0,
+                right: 0
+            },
+            web: {
+                bottom: 0,
+                left: 0
+            }
+        })
+    }
+})
